08fdac8fe227ef2f69af0cd86e1061497bacc2ce
gustaa23-beep <gutalexandre@hotmail.com>
2022-01-12 14:20:15 -0300

    Domain model, database seed

--- backend/src/main/java/com/devsuperior/dsmovie/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsmovie/config/SecurityConfig.java
@@ -14,7 +14,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-public class SecurityConfig {
+
 	@Configuration
 	@EnableWebSecurity
 	public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -42,4 +42,4 @@ public class SecurityConfig {
 			return source;
 		}
 	}
-}
+
